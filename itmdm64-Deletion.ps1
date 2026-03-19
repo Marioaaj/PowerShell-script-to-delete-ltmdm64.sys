0023ca14ccaabeb9f3a3a4a814f41e6799bdbdc7
Marioaaj <mario661962@yahoo.com>
2026-03-19 15:54:43 -0500

    Changed the location of readme itmdm64-Deletion.ps1

--- itmdm64-Deletion.ps1
+++ itmdm64-Deletion.ps1
@@ -1,4 +1,7 @@
+#This script was created in reponse to the article named "Patch Tuesday: Windows users hacked due to legacy fax modem driver" by Edward Targett 
+#He explains that there is a legacy file that appears on systems that can be used to hack stuff. This file is present on most windows computers.
 
+This was created to delete a vulnerability at the location. C:\Windows\System32\DriverStore\FileRepository\mdmagm64.inf_amd64_596c8fd6d9bb0499\ltmdm64.sys This was designed to be used with an RMM. It creates a report that includes the computer name it checks if the file exists at the location and it lets you know if you deleted the file.
 # --- Script to forcefully delete a file with elevated permissions ---
 
 
